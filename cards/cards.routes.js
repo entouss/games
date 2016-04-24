8c1379a9d3294cfd04b040d03ae0637b6d28b814
entouss <entouss@gmail.com>
2016-04-24 09:15:31 -0400

    Added files via upload
    relative path

--- cards/cards.routes.js
+++ cards/cards.routes.js
@@ -3,27 +3,27 @@
 		.config(function($routeProvider){
 			$routeProvider
 			.when('/cards',{
-				templateUrl : '/cards/cards.html',
+				templateUrl : 'cards/cards.html',
 				controller : 'CardsController',
 				controllerAs : 'cards'
 			})
 			.when('/cards/all',{
-				templateUrl : '/cards/cards.html',
+				templateUrl : 'cards/cards.html',
 				controller : 'CardsController',
 				controllerAs : 'cards'
 			})
 			.when('/cards/random/',{
-				templateUrl : '/cards/cards.html',
+				templateUrl : 'cards/cards.html',
 				controller : 'CardController',
 				controllerAs : 'cards'
 			})
 			.when('/cards/random/:count',{
-				templateUrl : '/cards/cards.html',
+				templateUrl : 'cards/cards.html',
 				controller : 'CardController',
 				controllerAs : 'cards'
 			})
 			.when('/cards/:id',{
-				templateUrl : '/cards/cards.html',
+				templateUrl : 'cards/cards.html',
 				controller : 'CardController',
 				controllerAs : 'cards'
 			})
