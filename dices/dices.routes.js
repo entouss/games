85f7cc1f4606c75cbfbbca28378808e7517cd650
entouss <entouss@gmail.com>
2016-04-24 09:15:11 -0400

    Added files via upload
    relative path

--- dices/dices.routes.js
+++ dices/dices.routes.js
@@ -3,27 +3,27 @@
 		.config(function($routeProvider){
 			$routeProvider
 			.when('/dices',{
-				templateUrl : '/dices/dices.html',
+				templateUrl : 'dices/dices.html',
 				controller : 'DicesController',
 				controllerAs : 'dices'
 			})
 			.when('/dices/all',{
-				templateUrl : '/dices/dices.html',
+				templateUrl : 'dices/dices.html',
 				controller : 'DicesController',
 				controllerAs : 'dices'
 			})
 			.when('/dices/random/',{
-				templateUrl : '/dices/dices.html',
+				templateUrl : 'dices/dices.html',
 				controller : 'DiceController',
 				controllerAs : 'dices'
 			})
 			.when('/dices/random/:count',{
-				templateUrl : '/dices/dices.html',
+				templateUrl : 'dices/dices.html',
 				controller : 'DiceController',
 				controllerAs : 'dices'
 			})
 			.when('/dices/:id',{
-				templateUrl : '/dices/dices.html',
+				templateUrl : 'dices/dices.html',
 				controller : 'DiceController',
 				controllerAs : 'dices'
 			})
